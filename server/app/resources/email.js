758e36329eb69098c4f73428984a59b3c70de880
Eugene Ware <eugene@noblesamurai.com>
2013-08-21 21:37:15 +1000

    Implementing Postgres REST

--- server/app/resources/email.js
+++ server/app/resources/email.js
@@ -1,25 +1,60 @@
 module.exports = function(models) {
+  var Email = models.email;
   return {
     index: function(req, res) {
-      res.send('email index');
+      Email.findAll()
+        .success(function (emails) {
+          res.json(emails);
+        })
+        .failure(function (err) {
+          res.json(500, { error: err.toString() });
+        });
     },
     new: function(req, res) {
       res.send('email new');
     },
     create: function(req, res) {
-      res.send('email create');
+      Email.create(req.body)
+        .success(function (email) {
+          res.json(email);
+        })
+        .failure(function (err) {
+          res.json(500, { error: err.toString() });
+        });
     },
     show: function(req, res) {
-      res.send('email show');
+      res.json(req.email);
     },
     edit: function(req, res) {
       res.send('email edit');
     },
     update: function(req, res) {
-      res.send('email update');
+      req.email.updateAttributes(req.body)
+        .success(function () {
+          res.json(req.email);
+        })
+        .failure(function (err) {
+          res.json(500, { error: err.toString() });
+        });
     },
     destroy: function(req, res) {
-      res.send('email destroy');
+      console.log('destroy?');
+      req.email.destroy()
+        .success(function () {
+          res.json({ msg: 'Item successfully deleted' });
+        })
+        .failure(function (err) {
+          res.json(500, { error: err.toString() });
+        });
+    },
+    load: function (id, cb) {
+      Email.find(id)
+        .success(function (email) {
+          cb(null, email);
+        })
+        .failure(function (err) {
+          cb(err);
+        });
     }
   }
 };
