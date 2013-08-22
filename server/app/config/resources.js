17c93cdf829c44d8cb56560ad9c6f8a5933d9ad0
Andrew Harris <andrew@noblesamurai.com>
2013-08-22 16:22:38 +1000

    re-arrange resources a little so they don't have to have a model

--- server/app/config/resources.js
+++ server/app/config/resources.js
@@ -6,16 +6,24 @@ module.exports = function(app, models) {
   // main resource
   app.resource(require(resourcesPath + '/main')(models));
 
+  // load model resources
+  for (var model in models) {
+    var resourceFile = resourcesPath + '/' + model + '.js';
+    if (!fs.exists(resourceFile)) {
+      resourceFile = resourcesPath + '/crud';
+    }
+    app.resource(model, require(resourceFile)(models, model));
+  }
+
   // load all other resources
-  fs.readdirSync(modelsPath).forEach(function(file) {
+  fs.readdirSync(resourcesPath).forEach(function(file) {
     var match = file.match(/(.*)\.js$/);
     if (match) {
-      var model = match[1];
-      var resourceFile = resourcesPath + '/' + model + '.js';
-      if (!fs.exists(resourceFile)) {
-        resourceFile = resourcesPath + '/crud';
+      var resource = match[1],
+          resourceFile = resourcesPath + '/' + file;
+      if (!models[resource] && ['main', 'crud'].indexOf(match[1]) < 0) {
+        app.resource(resource, require(resourceFile)(models));
       }
-      app.resource(model, require(resourceFile)(models, model));
     }
   });
 };
