f32f1c6a106175327591ea47c0b745bae298c1cf
Andrew Harris <andrew@noblesamurai.com>
2013-08-26 12:41:16 +1000

    angular base with less (rather than sass/compass).

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
