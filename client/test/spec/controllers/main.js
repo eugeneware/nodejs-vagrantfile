c1bcb0f5d5706e23752b6b394f209652378dd5c3
Andrew Harris <andrew@noblesamurai.com>
2013-08-28 13:47:51 +1000

    eql(==) => equal(===) again

--- client/test/spec/controllers/main.js
+++ client/test/spec/controllers/main.js
@@ -17,6 +17,6 @@ describe('Controller: MainCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).to.eql(3);
+    expect(scope.awesomeThings.length).to.equal(3);
   });
 });
